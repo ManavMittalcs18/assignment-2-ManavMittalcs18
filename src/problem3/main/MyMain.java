6919daad46e1cc9e9771c383866ece552bce38bd
ManavMittalcs18 <49266121+ManavMittalcs18@users.noreply.github.com>
2020-03-31 23:05:03 +0530

    Add files via upload

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,23 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+import problem5.student.Student;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String args[]) {
+        MyPriorityQueue obj = new MyPriorityQueue();
+        obj.insert("Yash",59);
+        obj.insert("Mohit",19);
+        obj.insert("Muskan",20);
+        obj.insert("Aishwarya",2);
+        obj.insert("Uddeshya",53);
+        System.out.println("Students according to their roll number are: ");
+        obj.peep();
+    }
 }
+
+
